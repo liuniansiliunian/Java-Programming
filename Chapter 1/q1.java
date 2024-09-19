5d1d746cd7c337f8fae5f407ac333a78d1943cdc
liuniansiliunian <652437157@qq.com>
2024-09-19 09:11:20 +0800

    Add files via upload

---
+++ Chapter 1/q1.java
@@ -0,0 +1,8 @@
+package exercise;
+
+public class q1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java,Welcome to Computer Science");
+        System.out.println("Programming is fun");
+    }
+}
