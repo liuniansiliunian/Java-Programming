b5806b618c3988b74147327320820362ffe45e17
liuniansiliunian <652437157@qq.com>
2024-10-21 11:21:13 +0800

    Update Task12.java

--- Week7/Task12.java
+++ Week7/Task12.java
@@ -10,23 +10,24 @@ public class Task12 {
         System.out.print("Enter full year (e.g., 2012): ");
         int year = sc.nextInt();
         System.out.print("Enter month as number between 1 and 12: ");
-        int month = sc.nextInt();
+        int month = sc.nextInt() - 1;
         printMonth(year, month);
         sc.close();
     }
 
     public static void printMonth(int year, int month) {
-        Calendar calendar = new GregorianCalendar(year, month - 1, 1);
+        Calendar calendar = new GregorianCalendar(year, month, 1);
         int firstDayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
 
-        System.out.println(year + " " + month + ":");
+        System.out.println(year + " " + (month + 1) + ":");
         System.out.println("  Sun Mon Tue Wed Thu Fri Sat");
 
-        int offset = Calendar.SUNDAY - firstDayOfWeek;
-        if (offset < 0) offset += 7;
+
+        int offset = firstDayOfWeek - Calendar.SUNDAY;
         for (int i = 0; i < offset; i++) {
             System.out.print("    ");
         }
+
 
         for (int day = 1; day <= calendar.getActualMaximum(Calendar.DAY_OF_MONTH); day++) {
             System.out.printf("%4d", day);
